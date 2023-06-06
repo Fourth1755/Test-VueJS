593a2c726d7cfd5bbf1748a2d2b47a3923310db4
fourth <fourthe.hero@gmail.com>
2023-06-06 13:26:37 +0700

    Update SearchBox.vue

--- client/src/components/SearchBox.vue
+++ client/src/components/SearchBox.vue
@@ -1,18 +1,47 @@
 <template>
-    <div>
-        <div>
+    <div class="search-container">
+        <div class="search-box">
             <input/>
+        </div>
+        <div>
+            
+        </div>
+        <div>
+            <SelectBoxDrpodown :list="country"/>
         </div>
     </div>
 </template>
 
 <script>
+import SelectBoxDrpodown from './SelectBoxDrpodown.vue';
 export default {
-    name:"SearchBox"
-
+    name:"SearchBox",
+    components:{
+        SelectBoxDrpodown
+    },
+    data(){
+        return{
+            country:["Korea","Thailand","Japan","USA","UK","Chaina","Cannada"]
+        }
+    }
 }
 </script>
 
 <style>
+    .search-container{
+        display: flex;
+        width: 100%;
+        height: 80px;
+        justify-content: space-between;
+    }
+    .search-box{
+        display: flex;
+        align-items: center;
+    }
+    .search-box input{
+        width: 420px;
+        height: 30px;
+    }
+
 
 </style>
